85987bba62bd6166fa3e434f405346872bb8c5b0
Chedro Cardenas <chegiorca@hotmail.com>
2019-12-01 11:47:31 -0700

    added nested Output and Input in order to display recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter,Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,24 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  
+  @Output() itemSelected = new EventEmitter<Recipe>();
+  
   recipes:Recipe[] = [
     new Recipe('Tacos','This is an special on the house, carnitas tacos','http://res.cloudinary.com/hksqkdlah/image/upload/v1481141685/29171_sfs-classic-beef-tacos-10.jpg',
     ),new Recipe('Costillas','this are ribs sesoned is with a traditional west sauce','https://spicysouthernkitchen.com/wp-content/uploads/slow-cooker-ribs-6.jpg'
     ) 
   ];//recipe of arrays of Recipes Objects define in the MODEL ../recipe/model
+
+  
   constructor() { }
 
   ngOnInit() {
   }
+  onRecipeSelected( recipe: Recipe){
+    this.itemSelected.emit(recipe);
+    console.log(this.itemSelected.emit(recipe))
+  }
+
 
 }
